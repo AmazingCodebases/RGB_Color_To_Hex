ac4ced926d5ea8224df6deb5f90a49dc33c20102
AmazingCodebases <rissmiller.m@icloud.com>
2025-11-27 15:50:57 -0500

    Add basic color input program in main.cpp

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+
+int Red_Input = 0;
+int Green_Input = 0;
+int Blue_Input = 0;
+
+
+
+int Exit = 0;
+
+
+string Red_Value_String = "";
+string Green_Value_String = "";
+string Blue_Value_String = "";
+
+
+
+
+Red_Value_String = "What Is Your Red Value?";
+Green_Value_String = "What Is Your Green Value?";
+Blue_Value_String = "What Is Your Blue Value?";
+
+
+
+cout << Red_Value_String << endl;
+cin >> Red_Input;
+
+cout << Green_Value_String << endl;
+cin >> Green_Input;
+
+cout << Blue_Value_String << endl;
+cin >> Blue_Input;
+
+//cout << "What Is Your Red Value?" << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cin >> Exit;
+return 0;
+}
